52d98488b7b4eeac4184d51f96f55c98074bfef6
artomesti <artomesti@gmail.com>
2025-07-07 06:45:45 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -6,4 +6,4 @@ export const terbit = '2025-04-21T00:01:03Z';
 export const google = 'knfXRfmRHSMRw30lAcfHPIfjS5cH-aUw7AmFKDBT3D4';
 export const yandex = 'c58c9034d24e0a97';
 
-export const index = '845edf13-d9f7-4eeb-89a0-ddb470df35d2';
+export const index = 'c99dccb3-2cf9-45b6-824b-c82b2a0c4c19';
